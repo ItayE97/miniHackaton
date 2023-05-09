fd823b687a35b7e162c6a20dc1bc97824a0c1b34
ItayE97 <evronitai@gmail.com>
2023-05-09 11:16:54 +0300

    added api page

--- server/server.js
+++ server/server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(logger);
+app.use(logger);
 
 app.listen(PORT, () => {    
     console.log(`Server listening to port ${PORT} on http://localhost:${PORT}`);
 });
 
 app.get("/",(req,res)=>{
-    res.send("Hello World!");
-    
+    res.send("Main page");
 });
+
+app.get("/api",(req,res)=>{
+    res.send("Welcome to the API page");
+});
+
+// const apiRouter = require('./routes/api');
+// app.use('/api', apiRouter);
+
+
+
+function logger(req,res,next){
+    console.log(req.originalUrl);
+    next();
+}
