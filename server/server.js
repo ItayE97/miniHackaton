78352e408134a4918cde36dffb511e2e3fb16815
Arad Ben Menashe <aradbm@gmail.com>
2023-05-09 12:22:28 +0300

    removed default css

--- server/server.js
+++ server/server.js
@@ -1,41 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
-
-app.listen(PORT, () => {    
-    console.log(`Server listening to port ${PORT} on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT} on http://localhost:${PORT}`);
 });
 
-app.get("/",(req,res)=>{
-    res.send("Main page");
+app.get("/", (req, res) => {
+  res.send("Main page");
 });
 
-app.get("/api",(req,res)=>{
-    res.json({ message: 'Hello from server!'});
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-app.post("/api",(req,res)=>{
-    res.json({ message: 'Post request!'});
+app.post("/api", (req, res) => {
+  res.json({ message: "Post request!" });
 });
 
 // const apiRouter = require('./routes/api');
 // app.use('/api', apiRouter);
 
-
-function logger(req,res,next){
-    console.log(req.originalUrl);
-    next();
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
 }
