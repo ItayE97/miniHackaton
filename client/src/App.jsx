b6218a243117e0fc787e46aaff74bad777abab4f
Arad Ben Menashe <aradbm@gmail.com>
2023-05-09 11:02:59 +0300

    added basic api fetch with button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Axios from "axios"
+import { Button } from "@mui/material";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, SetData] = useState("");
+  const fetchData = ()=>{
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/gaming/").then(
+      (res)=>{
+        // console.log(res.data[0].excuse)
+        SetData(res.data[0].excuse)
+      }
+    )
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={()=>fetchData()} >Fetch Data</button>
+      <Button>awesome button</Button>
+      <h1>Data: {data}</h1>
+    </div>
   )
 }
-
-export default App
+export default App;
